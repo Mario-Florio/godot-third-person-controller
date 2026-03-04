2784915b7be16502ed6180aa2bf94e5ba77e0cac
Mario <121590283+Mario-Florio@users.noreply.github.com>
2026-03-04 17:34:25 -0500

    Add input handling for vertical intent

--- src/scripts/Responsibilities/InputInterface.gd
+++ src/scripts/Responsibilities/InputInterface.gd
@@ -19,6 +19,7 @@ class IntentBearingInput extends RefCounted:
 	var _swap_shoulder: bool
 	var _move_vector: Vector2
 	var _speed_intent: StringName
+	var _jump: bool
 	
 	func _init(agentInputHandler: AgentInputHandler) -> void:
 		var agentIntent := AgentInputHandler.Snapshot.new(agentInputHandler)
@@ -27,6 +28,7 @@ class IntentBearingInput extends RefCounted:
 		_swap_shoulder = agentIntent.swap_shoulder
 		_move_vector = agentIntent.move_vector
 		_speed_intent = _convertSpeedIntent(agentIntent.speed_intent)
+		_jump = agentIntent.jump
 	
 	# Getters
 	func look_delta() -> Vector2:
@@ -43,6 +45,9 @@ class IntentBearingInput extends RefCounted:
 	
 	func speed_intent() -> StringName:
 		return _speed_intent
+	
+	func jump() -> bool:
+		return _jump
 	
 	# Utils
 	func _convertSpeedIntent(val: AgentInputHandler.SpeedIntent) -> StringName:
